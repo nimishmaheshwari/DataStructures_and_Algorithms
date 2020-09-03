9f3c3fab1ea31e4977218b4b3dfcdc5a18ea5c36
Nimish Maheshwari <nimishmaheshwari1998@gmail.com>
2020-09-03 14:46:41 +0530

    Added first pattern

---
+++ Patterns/First.py
@@ -0,0 +1,19 @@
+"""
+*
+**
+***
+****
+*****
+"""
+
+a=int(input("Enter the number of rows"))
+i=0
+while(i<a):
+    j=0
+    while(j<i+1):
+        print("*",end="")
+        j+=1
+    print()
+    i+=1
+
+
