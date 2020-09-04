289e730b1c70c32ab4f63b8144015f630b81bca2
Nimish Maheshwari <nimishmaheshwari1998@gmail.com>
2020-09-04 19:50:43 +0530

    sixth

---
+++ Patterns/sixth.py
@@ -0,0 +1,14 @@
+"""
+  * * * *
+ * * * *
+* * * *
+"""
+a=int(input("Enter the number of rows"))
+k=a-1
+for i in range(0,a):
+    for j in range(0,k):
+        print(end=" ")
+    k-=1
+    for n in range(0,a):
+        print("*",end=" ")
+    print()
