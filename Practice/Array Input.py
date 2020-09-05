1b720672bcac5f71a0e8afc1cd47fe5c2c00b0ce
Nimish Maheshwari <nimishmaheshwari1998@gmail.com>
2020-09-05 22:51:10 +0530

    Array Input

---
+++ Practice/Array Input.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the number of elements in the array"))
+lst=[]
+for i in range(0,a):
+    n=int(input("Enter the number of elements in array"))
+    lst1 = []
+    for j in range(0,n):
+        m=int(input( ))
+        lst1.append(m)
+    lst.append(lst1)
+    lst1=[]
+print(lst)
